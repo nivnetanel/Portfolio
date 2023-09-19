66861af377c90e2b38e2176793d847d611d4712f
niv netanel <nivnetanel@gmail.com>
2023-09-19 15:03:39 +0300

    form change

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,77 +1,66 @@
-import React, { Component } from "react";
+import React from "react";
+import { useForm, ValidationError } from "@formspree/react";
 import "../css/base.css";
 import "../css/main.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+
 AOS.init();
 
-class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <section id="contact" data-aos="zoom-in">
-          <div className="row section-intro">
-            <div className="col-twelve">
-              <h5>Contact</h5>
-              <h1>I'd Love To Hear From You.</h1>
-              <p className="lead"></p>
-            </div>
-          </div>
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mgejlzqr");
 
-          <div className="row contact-form">
-            <div className="col-twelve">
-              <form
-                target="_blank"
-                action="https://formsubmit.co/nivnetanel96@gmail.com"
-                method="POST"
-              >
-                <div className="form-group">
-                  <div className="form-row">
-                    <div className="col">
-                      <input
-                        type="text"
-                        name="name"
-                        className="form-control"
-                        placeholder="Full Name"
-                        required
-                      />
-                    </div>
-                    <div className="col">
-                      <input
-                        type="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email Address"
-                        required
-                      />
-                    </div>
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+
+  return (
+    <div>
+      <section id="contact" data-aos="zoom-in">
+        <div className="row section-intro">
+          <div className="col-twelve">
+            <h5>Contact</h5>
+            <h1>I'd Love To Hear From You.</h1>
+            <p className="lead"></p>
+          </div>
+        </div>
+
+        <div className="row contact-form">
+          <div className="col-twelve">
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <div className="form-row">
+                  <div className="col">
+                    {/* <label htmlFor="name">Full Name</label> */}
+                    <input id="name" placeholder="Full Name" type="text" name="name" className="form-control" required />
+                  </div>
+                  <div className="col">
+                    {/* <label htmlFor="email">Email Address</label> */}
+                    <input id="email" type="email" placeholder="Email Address" name="email" className="form-control" required />
+                    <ValidationError prefix="Email" field="email" errors={state.errors} />
                   </div>
                 </div>
-                <div className="form-group">
-                  <textarea
-                    placeholder="Your Message"
-                    className="form-control"
-                    name="message"
-                    rows="10"
-                    required
-                  ></textarea>
-                </div>
-                <button type="submit" className="submitform">
-                  Submit
-                </button>
-              </form>
-
-              <div id="message-warning"></div>
-              <div id="message-success">
-                <i className="fa fa-check"></i>Your message was sent, thank you!
-                <br />
               </div>
+              <div className="form-group">
+                {/* <label htmlFor="message">Your Message</label> */}
+                <textarea id="message" placeholder="Your Message" name="message" className="form-control" rows="10" required />
+                <ValidationError prefix="Message" field="message" errors={state.errors} />
+              </div>
+              <button type="submit" className="submitform" disabled={state.submitting}>
+                Submit
+              </button>
+            </form>
+
+            <div id="message-warning"></div>
+            <div id="message-success">
+              <i className="fa fa-check"></i>Your message was sent, thank you!
+              <br />
             </div>
           </div>
-        </section>
-      </div>
-    );
-  }
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Contact;
+export default ContactForm;
